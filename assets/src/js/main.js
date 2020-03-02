f9bcc136557bbb4f0a2c41c860bf9ce2c7e10475
Roman Chulin <chulin.roman2@gmail.com>
2020-03-02 18:38:41 +0700

    Added footer header and carousel wheel

---
+++ assets/src/js/main.js
@@ -0,0 +1,3 @@
+//= overlay.js
+//= slick.min.js
+  
---
+++ assets/src/js/overlay.js
@@ -0,0 +1,5 @@
+$("#navToggle").click(function() {
+    $(this).toggleClass("active");
+    $(".overlay").toggleClass("open");
+    $("body").toggleClass("locked"); 
+});
