24d07b3ff24069c7656010a5b47f31f4aaa62768
Roman Chulin <chulin.roman2@gmail.com>
2020-03-04 18:03:09 +0700

    Added yandex map and contact info

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -1,4 +1,4 @@
 //= overlay.js
 //= slick.min.js
 //= slick-config.js
-  
+//= anchor-animate.js
--- assets/src/js/slick-config.js
+++ assets/src/js/slick-config.js
@@ -1,7 +1,13 @@
 $(document).ready(function(){
   $('.carousel').slick({
     dots: true, 
-    //autoplay: true,
-    //autoplaySpeed: 2500
+    autoplay: true,
+    autoplaySpeed: 2500
+  });
+});
+
+$(document).ready(function(){
+  $('.feedback_carousel').slick({
+
   });
 });
