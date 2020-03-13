abfe26c952f079d80702cb77ffed6927b9c0faa7
Roman Chulin <chulin.roman2@gmail.com>
2020-03-13 17:52:05 +0700

    Done.

--- assets/src/js/slick-config.js
+++ assets/src/js/slick-config.js
@@ -4,10 +4,26 @@ $(document).ready(function(){
     autoplay: true,
     autoplaySpeed: 2500
   });
+  $('.modal__carousel').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false
+  });
+  $('.nav-carousel').slick({
+    asNavFor: '.modal__carousel',
+    arrows: false,
+    focusOnSelect: true,
+    adaptiveHeight: true,
+    slidesToShow: 3
+  });
 });
 
 $(document).ready(function(){
   $('.feedback_carousel').slick({
 
   });
+});
+
+$(document).ready(function(){
+
 });
